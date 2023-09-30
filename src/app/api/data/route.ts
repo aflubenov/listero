b33a298f8767a074143d53d37764049aefa28801
aflubenov <angelfernando@gmail.com>
2023-09-29 21:02:52 -0300

    Update route.ts v6

--- src/app/api/data/route.ts
+++ src/app/api/data/route.ts
@@ -57,8 +57,8 @@ export const POST = async (req: NextRequest) => {
     PGDATABASE: process.env.PGDATABASE,
     PGPASSWORD: process.env.PGPASSWORD,
     data: await getAll("alguna"),
-    data2: await saveData(data.institucion, data.mainInfo, []),
-    v:"5"
+    data2: await saveData(data.institucion, data.mainInfo, data.participantes),
+    v:"6"
   });
   /*
   if (!data || !data.institucion || !data.participantes || !data.mainInfo) {
