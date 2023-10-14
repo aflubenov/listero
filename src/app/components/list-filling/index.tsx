a4f8de288e4a6f983c5e764886113a1fefb6630e
angellubenovgrupogiga <angel.lubenov@grupo-giga.com>
2023-10-14 20:28:52 -0300

    Updating Version Number

--- src/app/components/list-filling/index.tsx
+++ src/app/components/list-filling/index.tsx
@@ -8,7 +8,7 @@ export const ListFilling = () => {
     const { dataDefinition, rowData,
         showPrint,
         storeData, addRow, downloadPDF, saveDataToServer, validateDataFromState,
-        formData, status, isValidData, removeRow } = useStorage("listero_v1.1_");
+        formData, status, isValidData, removeRow } = useStorage("listero_v1.2_");
 
     return (<>
         <div className='container-fluid bg-header pt-5'>
